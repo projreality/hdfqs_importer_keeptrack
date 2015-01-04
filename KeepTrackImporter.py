afdbd71606cc2e46d1764c42b229906ee10461ee
Samuel Li <sam@projreality.com>
2015-01-03 17:43:14 -0800

    Import data using default table settings.

--- KeepTrackImporter.py
+++ KeepTrackImporter.py
@@ -1,6 +1,6 @@
 import numpy as np;
 import re;
-from tables import *;
+from tables import Filters, Float64Col, Int64Col, Int8Col, openFile, exceptions;
 import xml.etree.ElementTree as et;
 
 class KeepTrackImporter:
@@ -12,6 +12,10 @@ class KeepTrackImporter:
     self.input_filename = config.input_filename;
     self.output_filename = config.output_filename;
     self.tz = config.tz / (60*15);
+
+    self.default_category = "Exercise";
+    self.default_numpy_type = np.float64;
+    self.default_pytables_type = Float64Col;
 
     self.filters = Filters(complevel=1, complib="zlib", shuffle=True, fletcher32=True);
 
@@ -36,18 +40,40 @@ class KeepTrackImporter:
       unit = watch.attrib["units"];
     except KeyError:
       unit = None;
-    
+
+    ( category, numpy_type, pytables_type ) = self._table_settings(name);
+
     data = [ ];
     for value in watch:
       tm = np.int64(value.attrib["time"])*1000000000;
 
       if (data_type == "number"):
-        val = np.float64(value.text);
+        val = numpy_type(value.text);
 
       if (data_type == "number"):
         data.append([ tm, self.tz, val ]);
       elif (data_type == "marker"):
         data.append([ tm, self.tz ]);
+
+    table_name = self._table_name(name);
+    if (len(data) == 0):
+      print("Skipping %s - no data." % ( name ));
+      return;
+    else:
+      print("Importing %s to /self/%s/%s" % ( name, category, table_name ));
+
+    try:
+      t = self.fd.getNode("/self/%s/%s" % ( self.default_category, table_name ));
+    except exceptions.NoSuchNodeError:
+      if (data_type == "number"):
+        descr = { "time": Int64Col(pos=0), "tz": Int8Col(pos=1), "value": pytables_type(pos=2) };
+      elif (data_type == "marker"):
+        descr = { "time": Int64Col(pos=0), "tz": Int8Col(pos=1) };
+      else:
+        print("Unknown KeepTrack data type: %s" % ( data_type ));
+        return;
+      t = self.fd.createTable("/self/Exercise", table_name, descr, name, filters=self.filters, createparents=True);
+      t.append(data);
 
   def _table_name(self, name):
     name = name.replace(" ", "").lower();
@@ -56,3 +82,6 @@ class KeepTrackImporter:
       return name;
     else:
       return "%s_%s" % ( x.group(1), x.group(2) );
+
+  def _table_settings(self, name):
+    return ( self.default_category, self.default_numpy_type, self.default_pytables_type );
